326f2facddb6243a4e93f07d9c3c21588848497e
integgy <matthewregidor@gmail.com>
2023-12-16 20:41:27 -0800

    create project index

--- frontend/src/components/Projects/ProjectIndex.jsx
+++ frontend/src/components/Projects/ProjectIndex.jsx
@@ -1,8 +1,30 @@
-function ProjectIndex () {
+import ProjectIndexItem from "./ProjectIndexItem"
+import "./ProjectIndex.css"
+
+const ProjectIndex = () => {
+    const projects = []
+
+    // Placeholder projects
+    for (let i = 1; i < 11; i++) {
+        projects.push(
+            {
+                id: i,
+                title: `TEST title ${i}`,
+                description: `TEST description ${i}`
+            }
+        )
+        
+    }
+    console.log(projects)
     return (
-        <>
-            <h1>test</h1>
-        </>
+        <ul className="project-index-container">
+            {projects.map(project => (
+                <ProjectIndexItem
+                    key={project.id}
+                    project={project}
+                />
+            ))}
+        </ul>
     )
 }
 
