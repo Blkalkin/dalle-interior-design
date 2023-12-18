8ca620f93112f3dcb9cad3bfa77f50f2a53bb3a3
Tatianna <tatirobinson@gmail.com>
2023-12-18 11:47:05 -0800

    Add validations (and errors) for user login/registration

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,8 @@ const User = mongoose.model('User');
 const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
 const { isProduction } = require('../../config/keys');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 
 
@@ -19,7 +21,7 @@ router.get('/', function(req, res, next) {
 
 
 
-router.post('/login', async (req, res, next) => {
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
@@ -34,7 +36,7 @@ router.post('/login', async (req, res, next) => {
 
 
 
-router.post('/register', async (req, res, next) => {
+router.post('/register', validateRegisterInput, async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
   const user = await User.findOne({
