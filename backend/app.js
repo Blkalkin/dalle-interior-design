fd441ba24749f6b80e1f6c9501aae36320b30d05
Tatianna <tatirobinson@gmail.com>
2023-12-18 10:20:06 -0800

    Enable CORS in development, add /api/csrf/restore for testing

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,38 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var usersRouter = require('./routes/api/users');
+const csurf = require('csurf');
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
 
-var app = express();
+const usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
+
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+if (!isProduction) {
+  app.use(cors());
+}
+
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
+
 app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
