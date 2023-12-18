f43e99562a390b41f1f11102db2e472ad450c4bc
Ashley <asyan77@gmail.com>
2023-12-18 15:33:57 -0800

    add before & after photos

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,34 @@
+import './LandingPage.css'
+import photo2 from '../../../assets/landingPagePhotos/1-livingroom-after.png'
+
+import photo1 from '../../../assets/landingPagePhotos/1-livingroom-before.jpeg'
 function LandingPage() {
     return (
-      <>
-        <p>Landing Page</p>
-        <footer>
-          Copyright &copy; 2023
-        </footer>
-      </>
+      <div className='whole-landing-page'>
+        
+        <div className='photos-container'>
+          {/* <div className='before-photos landing-photo'> */}
+            <img  className='landing-photo before' src={photo1} alt="before" />
+          {/* </div> */}
+          {/* <div className='after-photos landing-photo'> */}
+            <img className='landing-photo after' src={photo2} alt="after" />
+          {/* </div> */}
+        </div>
+
+        <div className='how-it-works-container'>
+            <div className='step-1'>
+
+            </div>
+            <div className='step-2'>
+
+            </div>
+            <div className='step 3'>
+
+            </div>
+        </div>
+        
+      </div>
+      
     );
   }
 
