cf6cdfbcb0a13fa0735c131667f641810c1d8bbb
Dylan <dylanquach@gmail.com>
2023-12-18 14:19:48 -0800

    Remove path for projects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,10 +44,6 @@ const router = createBrowserRouter([
         path: "profile",
         element: <Profile />
       },
-      {
-        path: "projects",
-        element: <ProjectIndex />
-      },
       // {
       //   path: "profile",
       //   element: <ProtectedRoute component={Profile} />
--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,34 +1,33 @@
 import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 // import { fetchUserTweets, clearTweetErrors, selectUserTweetsArray } from '../../store/tweets';
 // import TweetBox from '../Tweets/TweetBox';
 
 function Profile () {
-//   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 //   const currentUser = useSelector(state => state.session.user);
 //   const userTweets = useSelector(selectUserTweetsArray);
 
-//   useEffect(() => {
-//     dispatch(fetchUserTweets(currentUser._id));
-//     return () => dispatch(clearTweetErrors());
-//   }, [currentUser, dispatch]);
+  useEffect(() => {
+    dispatch(fetchUserTweets(currentUser._id));
+    return () => dispatch(clearTweetErrors());
+  }, [currentUser, dispatch]);
 
-//   if (userTweets.length === 0) {
-//     return <div>{currentUser.username} has no Tweets</div>;
-//   } else {
+  if (userTweets.length === 0) {
+    return <div>{currentUser.username} has no Tweets</div>;
+  } else {
     return (
       <>
-        {/* <h2>All of {currentUser.username}&apos;s Tweets</h2>
+        <h2>All of {currentUser.username}&apos;s Tweets</h2>
         {userTweets.map(tweet => (
           <TweetBox
             key={tweet._id}
             tweet={tweet}
           />
-        ))} */}
-        <h1>test1</h1>
+        ))}
       </>
     );
   }
-// }
+}
 
 export default Profile;
