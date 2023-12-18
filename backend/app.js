0f3dbff37bce9cdff22e8685d5aee13e5b7c6a44
Tatianna <tatirobinson@gmail.com>
2023-12-18 11:11:02 -0800

    Add login functionality

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ const cors = require('cors');
 const { isProduction } = require('./config/keys');
 
 require('./models/User');
+require('./config/passport');
+const passport = require('passport');
 
 const usersRouter = require('./routes/api/users');
 const csrfRouter = require('./routes/api/csrf');
@@ -21,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(passport.initialize());
 
 if (!isProduction) {
   app.use(cors());
