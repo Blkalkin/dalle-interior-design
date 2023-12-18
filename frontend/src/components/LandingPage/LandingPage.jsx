dbdc9280d30050ab29ac04fa513cc34d2b067a36
Ashley <asyan77@gmail.com>
2023-12-18 15:58:48 -0800

    add How it Works section with placeholder text

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,9 @@
 import './LandingPage.css'
 import photo2 from '../../../assets/landingPagePhotos/1-livingroom-after.png'
-
 import photo1 from '../../../assets/landingPagePhotos/1-livingroom-before.jpeg'
+
+
+
 function LandingPage() {
     return (
       <div className='whole-landing-page'>
@@ -15,14 +17,20 @@ function LandingPage() {
           {/* </div> */}
         </div>
 
-        <div className='how-it-works-container'>
-            <div className='step-1'>
+        <div className='how-it-works-container text'> How it works
+            <div className='step-1 steps'>
+              <div className='count'>01</div>
+              <div className='steps-text text'>Create a new project by uploading a photo</div>
 
             </div>
-            <div className='step-2'>
+            <div className='step-2 steps'>
+              <div className='count'>02</div>
+              <div className='steps-text text'>Make edits to your photo. Save the photos you like inside your project portfolio</div>
 
             </div>
-            <div className='step 3'>
+            <div className='step 3 steps'>
+              <div className='count'>03</div>
+              <div className='steps-text text'>Share your portfolio to the community! View and comment on projects from your fellow community </div>
 
             </div>
         </div>
