91000fcc330988d08c8853dc775aee23a6ded13e
Dylan <dylanquach@gmail.com>
2023-12-19 14:21:59 -0800

    test 4

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -37,6 +37,7 @@ router.post('/login', validateLoginInput, async (req, res, next) => {
 console.log("test")
 console.log("test 2")
 console.log("test 3")
+console.log("test 4")
 
 
 router.post('/register', validateRegisterInput, async (req, res, next) => {
