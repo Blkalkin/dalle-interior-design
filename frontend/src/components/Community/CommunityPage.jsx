b562847e747c8b252f3a1a510a1418d1455362c5
Dylan <dylanquach@gmail.com>
2023-12-19 09:45:40 -0800

    Small change to image links

--- frontend/src/components/Community/CommunityPage.jsx
+++ frontend/src/components/Community/CommunityPage.jsx
@@ -1,32 +1,10 @@
-import ProjectIndexItem from "../Projects/ProjectIndexItem";
+import ProjectIndex from "../Projects/ProjectIndex";
 import './Community.css';
 
 function CommunityPage () {
-    const projects = []
-
-    for (let i = 1; i < 11; i++) {
-        projects.push(
-            {
-                id: i,
-                title: `TEST title ${i}`,
-                description: `TEST description ${i}`
-            }
-        )
-    }
-
     return (
         <>
-            <div className="project-index-container">
-                <h2>Community Page</h2>
-                <ul className="projects-index">
-                    {projects.map(project => (
-                        <ProjectIndexItem
-                            key={project.id}
-                            project={project}
-                        />
-                    ))}
-                </ul>
-            </div>
+            <ProjectIndex title="community projects"/>
         </>
     )
 }
