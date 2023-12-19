3b937bf62c359bd1bc658aa2f23a9ca82956eca5
Ashley <asyan77@gmail.com>
2023-12-18 16:48:49 -0800

    add rotating features for photos. Waiting on photos to test

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,43 +1,60 @@
 import './LandingPage.css'
-import photo2 from '../../../assets/landingPagePhotos/1-livingroom-after.png'
-import photo1 from '../../../assets/landingPagePhotos/1-livingroom-before.jpeg'
+import photo0 from '../../../assets/landingPagePhotos/1-livingroom-before.jpeg'
+import photo1 from '../../../assets/landingPagePhotos/1-livingroom-after.png'
+import { useCallback, useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 
 function LandingPage() {
-    return (
-      <div className='whole-landing-page'>
-        
-        <div className='photos-container'>
-          {/* <div className='before-photos landing-photo'> */}
-            <img  className='landing-photo before' src={photo1} alt="before" />
-          {/* </div> */}
-          {/* <div className='after-photos landing-photo'> */}
-            <img className='landing-photo after' src={photo2} alt="after" />
-          {/* </div> */}
-        </div>
+  const loggedIn = useSelector(state => !!state.session.user);
+  const [beforeImg, setBeforeImg] = useState(0)
+  const [afterImg, setAfterImg] = useState(1)
+  const imagesArr = [photo0, photo1]
 
-        <div className='how-it-works-container text'> How it works
-            <div className='step-1 steps'>
-              <div className='count'>01</div>
-              <div className='steps-text text'>Create a new project by uploading a photo</div>
+  const rotateImg = useCallback(() => {
+  const beforeIndex = (beforeImg + 2) % imagesArr.length
+  setBeforeImg(beforeIndex);
 
-            </div>
-            <div className='step-2 steps'>
-              <div className='count'>02</div>
-              <div className='steps-text text'>Make edits to your photo. Save the photos you like inside your project portfolio</div>
+  const afterIndex = (afterImg + 2) % imagesArr.length
+  setAfterImg(afterIndex);
+  }, [imagesArr.length, beforeImg, afterImg]);
 
-            </div>
-            <div className='step 3 steps'>
-              <div className='count'>03</div>
-              <div className='steps-text text'>Share your portfolio to the community! View and comment on projects from your fellow community </div>
+  useEffect(() => {
+  const intervalID = setInterval(rotateImg, 5000);
+  return () => clearInterval(intervalID);
+  }, [rotateImg])
 
-            </div>
-        </div>
-        
+  if (loggedIn) return <Navigate to='/community' />
+
+  return (
+    <div className='whole-landing-page'>
+
+      <div className='photos-container'>
+        <img className='landing-photo before' src={imagesArr[beforeImg]} alt="before" />
+        <img className='landing-photo after' src={imagesArr[afterImg]} alt="after" />
+       </div>
+
+    <div className='how-it-works-container text'> How it works
+      <div className='step-1 steps'>
+         <div className='count'>01</div>
+         <div className='steps-text text'>Create a new project by uploading a photo</div>
       </div>
-      
-    );
+
+      <div className='step-2 steps'>
+         <div className='count'>02</div>
+         <div className='steps-text text'>Make edits to your photo. Save the photos you like inside your project portfolio</div>
+      </div>
+
+      <div className='step 3 steps'>
+        <div className='count'>03</div>
+        <div className='steps-text text'>Share your portfolio to the community! View and comment on projects from your fellow community </div>
+      </div>
+    </div>
+
+    </div>
+  );
   }
 
-  export default LandingPage;
+export default LandingPage;
