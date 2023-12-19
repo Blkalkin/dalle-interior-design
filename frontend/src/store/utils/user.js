94c57d63a79c46795d8a7697691d0b7376b7f87e
Dylan <dylanquach@gmail.com>
2023-12-18 17:41:01 -0800

    Fix user profile

--- frontend/src/store/utils/user.js
+++
@@ -1,9 +0,0 @@
-import jwtFetch from '../jwt';
-
-export const getUser = (userId) => returnUser(userId, `api/users/${userId}`)
-
-const returnUser = (userId, route) = async dispatch => {
-    const res = await jwtFetch(route)
-    const user = await res.json();
-    return user
-}
