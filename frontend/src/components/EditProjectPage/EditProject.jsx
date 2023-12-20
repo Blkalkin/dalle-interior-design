ccc87b721e7993d10da16ce8e3b90f37ee39d44e
integgy <matthewregidor@gmail.com>
2023-12-20 05:24:11 -0800

    render test

--- frontend/src/components/EditProjectPage/EditProject.jsx
+++ frontend/src/components/EditProjectPage/EditProject.jsx
@@ -6,14 +6,7 @@ import { useEffect, useState } from "react"
 import RecentPicture from './RecentPicture';
 
 function EditProject () {
-    const obj = {
-        _id: "658218b08522932af6596a6a",
-        author: "658218af8522932af6596a62",
-        title: "Tactus depereo esse corporis tenuis barba adeo desparatus cunctatio ta…",
-        description: "Vos vulnus audentia aestivus culpa.",
-        photoUrls: ["hi", "hi2"],
-        public: true
-    }
+
     const [description, setDescription] = useState("test");
 
     const { id } = useParams();
