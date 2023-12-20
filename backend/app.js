40cf5e76952d69324002ad38a82916e88428a329
Blkalkin <88650606+Blkalkin@users.noreply.github.com>
2023-12-20 10:59:09 -0800

    finish route, refactor

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const debug = require('debug');
+require('dotenv').config();
 
 
 const csurf = require('csurf');
--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -1,13 +1,136 @@
 const express = require('express');
 const router = express.Router();
 const { OpenAI } = require('openai');
+const fs = require('fs');
+const Jimp = require('jimp');
+require('dotenv').config({ path: '../../.env' });
 
 
-const apiKey =  "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"
 
-const openai = new OpenAI({apiKey: apiKey});
+class ImageProcessor {
+    constructor() {
+      this.openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+    }
+  
+    async processImage(imagePath, outputPath) {
+        try {
+            // Read and crop the image
+            if (!fs.existsSync(imagePath)) {
+                throw new Error(`File not found at ${imagePath}`);
+            }
+            let image = await Jimp.read(imagePath);
+            const minDimension = Math.min(image.bitmap.width, image.bitmap.height);
+            const cropX = (image.bitmap.width - minDimension) / 2;
+            const cropY = (image.bitmap.height - minDimension) / 2;
+            image = await image.crop(cropX, cropY, minDimension, minDimension);
+            await image.writeAsync("image_generation/route-images/cropped_image.png");
+            console.log("Image processed and saved to: cropped_image.png");
+      
+            // Calculate the size of the transparent square (80% of the cropped image)
+            const squareSize = Math.floor(minDimension * 0.8);
+            const offsetX = Math.floor((minDimension - squareSize) / 2);
+            const offsetY = Math.floor((minDimension - squareSize) / 2);
+      
+            // Make the center square transparent
+            for (let x = offsetX; x < offsetX + squareSize; x++) {
+                for (let y = offsetY; y < offsetY + squareSize; y++) {
+                    image.setPixelColor(Jimp.rgbaToInt(0, 0, 0, 0), x, y);  // Set to fully transparent
+                }
+            }
+      
+            // Save the processed image
+            await image.writeAsync(outputPath);
+            console.log('Image processed and saved to:', outputPath);
+        } catch (error) {
+            console.error('Error processing image:', error);
+        }
+    }
+  
+    async generateImageEdit(imagePath, base64Image, userPrompt) {
+        const response = await this.openai.chat.completions.create({
+            model: "gpt-4-vision-preview",
+            max_tokens: 4096,
+            messages: [
+              {
+                role: "system",
+                content: `
+                Analyze the image and concisely describe key elements with high realism. Focus on the room's ambiance, layout, size, ceiling height, and the photo's angle, explaining its impact on object visibility and arrangement.
+        
+        Detail the color scheme and lighting, whether natural or artificial, dim or bright. Describe the flooring, like wooden floorboards or marble tiles, and any rugs, noting their patterns and textures.
+        
+        Enumerate furniture pieces, specifying styles, colors, materials, and positions relative to the camera's perspective (e.g., a forest green velvet armchair in the foreground left). Highlight decorative items such as paintings, sculptures, or plants, including sizes, colors, positions, and textures.
+        
+        Describe visible appliances and electronics, stating brands, models, and conditions, along with wall hangings, window treatments, or bookshelves, and their contents.
+        
+        Comment on the room's overall condition (tidy, cluttered, pristine, worn) and notable features like fireplaces or beams. Include sensory details and emotional responses they evoke.
+        
+        Use the user's input to guide any modifications in the room's design, focusing on specific areas for inpainting. Keep the language clear and within safety guidelines, aiming for a description under 1000 characters. Do not address the user at any point, the final output must be a description and nothing futher.
+                `
+              },
+              {
+                role: "user",
+                content: [
+                  { type: "text", text: userPrompt + "this response must be kept under 900 characters"},
+                  {
+                    type: 'image_url',
+                            image_url: {
+                                url: `data:image/jpeg;base64,${base64Image}`
+                            }
+                  },
+                ],
+              },
+            ],
+          });
+          let response_text = response.choices[0].message.content;
+          let formatted_text = response_text.replace(/\n/g, ' ');
+          console.log(formatted_text)
+        
+          const baseImageStream = fs.createReadStream('image_generation/route-images/cropped_image.png');
+          const maskImageStream = fs.createReadStream('image_generation/route-images/output.png');
+        
+          const image_generated = await this.openai.images.edit({
+            image: baseImageStream,
+            mask: maskImageStream,
+            prompt: formatted_text
+          });
+          console.log(image_generated)
+     
+    }
 
-router.post('/generate', async (req, res) => {
+    async generateImagePrompt(imagePath,userPrompt){
+    
+    }
+
+    
+  
+    static imageToBase64(path) {
+      const image = fs.readFileSync(path);
+      return image.toString('base64');
+    }
+  }
+
+
+const imageProcessor = new ImageProcessor();
+
+
+router.post('/generate-image', async (req, res) => {
+    try {
+      const { imagePath, userPrompt } = req.body;
+      const outputPath = 'image_generation/route-images/output.png';
+  
+      await imageProcessor.processImage(imagePath, outputPath);
+      const base64Image = ImageProcessor.imageToBase64(imagePath);
+  
+      const response = await imageProcessor.generateImageEdit(imagePath, base64Image, userPrompt);
+      res.json(response);
+    } catch (error) {
+      console.error('Error in route:', error);
+      res.status(500).send('An error occurred');
+    }
+});
+
+
+router.post('/generate-prompt-image', async (req, res) => {
     const { imageUrl, promptText } = req.body;
 
     try {
