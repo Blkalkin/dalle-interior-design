3bd706451e992076ebc338f0a6644709a1342d5e
Dylan <dylanquach@gmail.com>
2023-12-21 08:20:20 -0800

    Create star button

--- frontend/src/components/EditProjectPage/RecentPicture.jsx
+++ frontend/src/components/EditProjectPage/RecentPicture.jsx
@@ -1,17 +1,30 @@
 import './EditProject.css'
 
 function RecentPicture ({photoUrls, newImage}) {
+
+  const handleClick = (boxName) => {
+    if (boxName === 'secondBox') {
+      // Clone the current array and add the newImage to the end
+      const updatedUrls = [...photoUrls, newImage];
+      // Perform any additional logic here if needed
+      console.log('Updated URLs:', updatedUrls);
+    }
+  };
+
     return (
-        <>
-            <div class="image-container">
-                <div className="image-box" onclick="handleClick('firstBox')">
-                    Image Box 1
-                </div>
-                <div className="image-box" onclick="handleClick('secondBox')">
-                    Image Box 2
-                </div>
-            </div>
-        </>
+    <>
+      <div className="image-container">
+        <div className="image-box" onClick={() => handleClick('firstBox')}>
+          Image Box 1
+        </div>
+        <div className="image-box" onClick={() => handleClick('secondBox')}>
+          Image Box 2
+          <button onClick={() => handleClick('secondBox')}>
+            Star
+          </button>
+        </div>
+      </div>
+    </>
     )
 }
 
