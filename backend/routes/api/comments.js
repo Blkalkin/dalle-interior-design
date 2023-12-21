6e04d656a71d5225477de0e40932c994b3489f6b
Tatianna <tatirobinson@gmail.com>
2023-12-21 11:22:22 -0800

    Add PATCH route for comments

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -63,6 +63,41 @@ router.post('/', async (req, res, next) => {
       }
 );
 
+router.patch('/:commentId/edit', async (req, res, next) => {
+  let comment;
+
+  try{
+    comment = await Comment.findById(req.params.commentId);
+  }catch(err) {
+    const error = new Error('Comment not found');
+    error.statusCode = 404;
+    error.errors = { message: "No comment found with that id" };
+    return next(error);
+  }try {
+
+    comment.body = req.body.body || comment.body.body;
+    let editedComment = await comment.save();
+    editedComment = await comment.populate('author', '_id');
+    return res.json(editedComment);
+  }catch(err) {
+    if (err.name === 'ValidationError') {
+      // Mongoose validation error
+      const validationErrors = {};
+      for (const field in err.errors) {
+        validationErrors[field] = err.errors[field].message;
+      }
+      const error = new Error('Validation failed');
+      error.statusCode = 422;
+      error.errors = validationErrors;
+      return next(error);
+    }
+    // Handle other Mongoose errors
+    const error = new Error('Comment failed to save');
+    error.statusCode = 500; // Internal Server Error
+    return next(error);
+  }
+})
+
 router.delete('/:commentId', async (req, res, next) => {
 
   try {
