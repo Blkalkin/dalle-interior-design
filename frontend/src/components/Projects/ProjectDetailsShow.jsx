4eaf1ba2bb3a779735ff31162930229371a60e74
Ashley <asyan77@gmail.com>
2023-12-20 17:13:59 -0800

    ready to add update and delect functionality and components

--- frontend/src/components/Projects/ProjectDetailsShow.jsx
+++ frontend/src/components/Projects/ProjectDetailsShow.jsx
@@ -1,15 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Link, useParams } from "react-router-dom"
 import { fetchProject, selectProject } from "../../store/project"
 import './ProjectDetailsShow.css'
 import CommentIndex from "../Comments/CommentIndex"
+import editIcon from '../../../assets/icons/editIcon.png'
 
 const ProjectDetailsShow = () =>{
     const dispatch = useDispatch()
     const {projectId} = useParams()
     const project = useSelector(selectProject(projectId))
     const photos = project?.photoUrls
+    const currUser = useSelector(state => state.session.user)
+    const [isCurrUser, setIsCurrUser] = useState(false)
+
+    if(project?.author === currUser._id) {
+        setIsCurrUser(true)
+    }
     
     useEffect( ()=> {
         dispatch(fetchProject(projectId))
@@ -18,14 +25,17 @@ const ProjectDetailsShow = () =>{
 
     console.log(projectId)
     console.log(project, "PROJECT")
-    console.log(photos, "photos")
-    // if(!photos) return null 
+    console.log(isCurrUser)
+
 
     if(project) {
         return (
             <div className="project-details-page">
                <div className="project-title-PDS title">{project.title}</div>
                <div className="project-description-PDS text">{project.description}</div>
+               {currUser._id !== project.author ? 
+                 <img className='edit-PDS' src={editIcon} alt="" />
+               : null}
                <div className="photos-and-comments-comtainer">
                     <ul className="projects-index-grid-PDS">
                         {photos.map((photo, idx)=> {
