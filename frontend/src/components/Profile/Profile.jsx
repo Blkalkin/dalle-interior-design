4e18ba605034914fd3dd4f45eca2a364d9602011
integgy <matthewregidor@gmail.com>
2023-12-21 21:57:47 -0800

    create error handle for profile not found

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,47 +1,46 @@
 import './Profile.css'
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { Navigate, Link } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import ProjectIndex from '../Projects/ProjectIndex';
-import { getUser } from '../../store/user';
-import { useEffect, useState } from "react"
+import { fetchUser } from '../../store/user';
+import { useEffect } from "react"
 
 
 function Profile () {
     const { userId } = useParams();
     const dispatch = useDispatch();
-    const [profileOwner, setProfileOwner] = useState(false)
+    const navigate = useNavigate()
     const currUser = useSelector(state => state.session.user)
     const user = useSelector(state => state.users[userId])
-
+    const profileOwner = currUser && currUser._id === userId
+    
     const capitalizeFirstLetter = str => {
         if (str) return str.charAt(0).toUpperCase() + str.slice(1); 
     }
 
-    
     useEffect (() => {
-        if (currUser._id === userId) {
-            setProfileOwner(true)
-        } else {
-            dispatch(getUser(userId))
-        }
-
-    }, [dispatch, userId, profileOwner, currUser])
+        dispatch(fetchUser(userId))
+    }, [dispatch, userId, navigate])
 
 
-
-    return (
-        <div className='profile-container text'>
-            <h2>
-                {profileOwner? `${capitalizeFirstLetter(currUser.username)}` : `${capitalizeFirstLetter(user?.username)}`}
-            </h2>
-                {profileOwner? (
-                    <ProjectIndex currentUser={currUser} projectOwner={profileOwner}/>
-                ):(
-                    <ProjectIndex user={user}/>
-                )}
-        </div>
-    )
+    if (currUser || user) {
+        return (
+            <div className='profile-container'>
+                <h2 className='profile-title title'>
+                    {profileOwner? (
+                        `${capitalizeFirstLetter(currUser.username)}'s Projects`
+                        ) : (
+                            user? (`${capitalizeFirstLetter(user.username)}'s Projects`) : "404 Profile NOT FOUND"
+                            )}
+                </h2>
+                    {profileOwner? (
+                        <ProjectIndex currentUser={currUser} projectOwner={profileOwner}/>
+                    ):(
+                        <ProjectIndex user={user}/>
+                    )}
+            </div>
+        )
+    }
 
 }
 
