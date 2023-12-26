bf0bd1fe0538f519a90aebe3ea37c45287796e1c
integgy <matthewregidor@gmail.com>
2023-12-25 20:54:08 -0800

    create project filter component

--- frontend/src/components/Community/CommunityFilter.jsx
+++ frontend/src/components/Community/CommunityFilter.jsx
@@ -1,13 +1,34 @@
+import { Link } from 'react-router-dom'
+import office from '../../../assets/landingPagePhotos/2-office-before.jpeg'
+import living from "../../../assets/landingPagePhotos/1-livingroom-before.jpeg"
+import bathroom from "../../../assets/landingPagePhotos/3-bathroom-after.png"
+import bedroom from "../../../assets/landingPagePhotos/4-kids-room-after.png"
+import dinning from "../../../assets/project-filter-images/dinning room.png"
+import kitchen from "../../../assets/project-filter-images/kitchen.png"
+import entryway from "../../../assets/project-filter-images/entryway.png"
+import laundry from "../../../assets/project-filter-images/laundry.png"
+
+import "./CommunityFilter.css"
 const CommunityFilter = () => {
     const categories = [
-        "kitchen", "dinning", "bath", 
-        "bedroom", "living", "dinning", 
-        "office", "entry", "laundry"
+        {"type":"kitchen", "image": kitchen},
+        {"type": "bath", "image": bathroom}, 
+        {"type": "bedroom", "image": bedroom}, 
+        {"type": "living", "image": living}, 
+        {"type": "dinning", "image": dinning}, 
+        {"type": "office", "image": office}, 
+        {"type": "entry", "image": entryway}, 
+        {"type": "laundry", "image": laundry}
     ]
 
     return (
         <div className="community-filter-container">
-            
+            {categories.map((category,idx) => 
+                <Link className='community-filter-item'>
+                    <img src={category.image} alt="" />
+                    <h3 className='title'>{category.type}</h3>
+                </Link>
+            )}
         </div>
     )
 }
--- frontend/src/components/Community/CommunityPage.jsx
+++ frontend/src/components/Community/CommunityPage.jsx
@@ -1,14 +1,17 @@
 import ProjectIndex from "../Projects/ProjectIndex";
 import './Community.css';
+import CommunityFilter from "./CommunityFilter";
 
 function CommunityPage () {
 
     return (
-        <div className="community-container">
-
-            <h1 className="community-title title">Community Projects</h1>
-            <ProjectIndex/>
-        </div>
+        <>
+                <CommunityFilter/>
+            <div className="community-container">
+                <h1 className="community-title title">Community Projects</h1>
+                <ProjectIndex/>
+            </div>
+        </>
     )
 }
 
