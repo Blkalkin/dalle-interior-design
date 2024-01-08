e9dc0a376ebfbad3ee7fcf967e9371cea0eadc89
Ashley <asyan77@gmail.com>
2024-01-08 14:28:18 -0800

    edit text/buttons on CreateProject.jsx

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -42,16 +42,21 @@ function LandingPage() {
     <div className='how-it-works-container text'> How it works
       <div className='step-1 steps'>
          <div className='count'>01</div>
-         <div className='steps-text text'>Create a new project by uploading a photo</div>
+         <div className='steps-text text'>Create a new project by uploading your own photo, or start off with one of our demo photos. </div>
       </div>
 
       <div className='step-2 steps'>
          <div className='count'>02</div>
-         <div className='steps-text text'>Make edits to your photo. Save the photos you like inside your project portfolio</div>
+         <div className='steps-text text'>Choose your vibe... Feeling wild? Go for the creative option. Feeling decisive? Choose the standard option, tell us what/how/where you want your upgrades, the more details the better! </div>
       </div>
 
-      <div className='step 3 steps'>
-        <div className='count'>03</div>
+      <div className='step-3 steps'>
+         <div className='count'>03</div>
+         <div className='steps-text text'> Star and save the photos you like. One you're satisfied, click done to view your newly created project portfolio</div>
+      </div>
+
+      <div className='step-4 steps'>
+        <div className='count'>04</div>
         <div className='steps-text text'>Share your portfolio to the community! View and comment on projects from your fellow community </div>
       </div>
     </div>
