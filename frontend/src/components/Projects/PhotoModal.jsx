f378b4386131c7be4f7811178013c4031672f038
integgy <matthewregidor@gmail.com>
2024-01-09 14:00:08 -0800

    fix create project image size

--- frontend/src/components/Projects/PhotoModal.jsx
+++ frontend/src/components/Projects/PhotoModal.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useRef } from 'react'
 import './PhotoModal.css'
 
 
 const PhotoModal = (props) => {
+    const modalRef = useRef(null)
     console.log("url in modal component: ", props.url)
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+          if (modalRef.current && !modalRef.current.contains(event.target)) {
+            props.closePhotoModal()
+          }
+        };
+
+        document.addEventListener("mousedown", handleOutsideClick);
+        document.body.style.overflow = "hidden"
+
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+            document.body.style.overflow = ""
+        };
+
+      }, [props.closePhotoModal]);
+
     return (
         <div className='modal-background'>
-            <div className='modal-content'>
+            <div className='modal-content' ref={modalRef}>
                 <div>
                     <span className="close" onClick={props.closePhotoModal}>&times;</span>
                     <img className='enlarged-photo' src={props.url} alt="" />
