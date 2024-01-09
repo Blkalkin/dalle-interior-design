d7dd3bc97b93e31e2c4983dc4ddead394553d81e
Ashley <asyan77@gmail.com>
2024-01-08 18:59:07 -0800

    how it works steps update, plus project grid is x4

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -42,17 +42,22 @@ function LandingPage() {
     <div className='how-it-works-container text'> How it works
       <div className='step-1 steps'>
          <div className='count'>01</div>
-         <div className='steps-text text'>Create a new project by uploading a photo</div>
+         <div className='steps-text text'>Create a new project by uploading your photo, or get started with one of our sample images</div>
       </div>
 
       <div className='step-2 steps'>
          <div className='count'>02</div>
-         <div className='steps-text text'>Make edits to your photo. Save the photos you like inside your project portfolio</div>
+         <div className='steps-text text'>Decide your vibe! Design your room/photo using our Creative or Standard mode, up to 50 edits per day!</div>
+      </div>
+
+      <div className='step-3 steps'>
+         <div className='count'>03</div>
+         <div className='steps-text text'>Once you're satisfied with your results, click to save your project and view it in your project portfolio </div>
       </div>
 
       <div className='step 3 steps'>
-        <div className='count'>03</div>
-        <div className='steps-text text'>Share your portfolio to the community! View and comment on projects from your fellow community </div>
+        <div className='count'>04</div>
+        <div className='steps-text text'>Check out other projects! View and comment on fellow projects from the Roominate community </div>
       </div>
     </div>
 
