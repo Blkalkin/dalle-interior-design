7c082381723f3a83ff2c1f7909be948e1473ffc8
Dylan <dylanquach@gmail.com>
2024-01-10 17:09:48 -0800

    Fix example images styling

--- frontend/src/components/Projects/ExampleImagesModal.jsx
+++ frontend/src/components/Projects/ExampleImagesModal.jsx
@@ -1,20 +1,21 @@
 import "./CreateProjectModal.css"
+import test from './images/test1.png'
 
 const ExampleModal = () => {
     return (
         <>
             <div className="create-project-modal-right">
                 <div className="create-project-modal-content">
+
                     <div className="create-modal-step-1">
-                        <div className="text">
+                        <div className="text-ex text">
                             Example Images (Drag)
                         </div>
-                        <ul className="example-image-list">
-                            <img src="../images/test1.png" alt="" />
-                            <img src="../images/test1.png" alt="Example 2" />
-                            <img src="images/test1.png" alt="" />
-                            {/* Add more examples as needed */}
-                        </ul>
+                        <div className="images-container">
+                            <img src={test} alt="Error loading" />
+                            <img src={test} alt="Error loading" />
+                            <img src={test} alt="Error Loading" />
+                        </div>
                     </div>
                 </div>
             </div>
