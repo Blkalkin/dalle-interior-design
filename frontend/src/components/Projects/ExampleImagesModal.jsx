43984cad094398248e1fc38bc4d63555fcdf9a34
Dylan <dylanquach@gmail.com>
2024-01-10 16:40:34 -0800

    Add images

--- frontend/src/components/Projects/ExampleImagesModal.jsx
+++ frontend/src/components/Projects/ExampleImagesModal.jsx
@@ -3,11 +3,18 @@ import "./CreateProjectModal.css"
 const ExampleModal = () => {
     return (
         <>
-            <div className="create-project-modal">
+            <div className="create-project-modal-right">
                 <div className="create-project-modal-content">
                     <div className="create-modal-step-1">
-                        <h1>Hi</h1>
-                        {/* <FilesDragAndDrop setImage={setImage}/> */}
+                        <div className="text">
+                            Example Images (Drag)
+                        </div>
+                        <ul className="example-image-list">
+                            <img src="../images/test1.png" alt="" />
+                            <img src="../images/test1.png" alt="Example 2" />
+                            <img src="images/test1.png" alt="" />
+                            {/* Add more examples as needed */}
+                        </ul>
                     </div>
                 </div>
             </div>
